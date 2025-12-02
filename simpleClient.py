2c0f16c8c3397b17b38c44fdbbd33cc01f1bd0aa
monogatari.ronin <monogatari99@googlemail.com>
2025-12-02 13:52:12 +0100

    update

--- simpleClient.py
+++ simpleClient.py
@@ -15,35 +15,42 @@ CONFIG = {
         "hash": {
             "command": "uv",
             "args": ["run", server_path]
-        },
-        "weather": {
-            "command": "uv",
-            "args": [
-                "--directory",
-                ".",
-                "run",
-                "simpleServerWeather.py"
-            ]
+#            "args": [
+#                "--directory",
+#                ".",
+#                "run",
+#                "simpleServerHash.py"
+#            ]
         }
+#        ,
+#        "weather": {
+ #           "command": "uv",
+ #           "args": [
+ #               "--directory",
+ #               ".",
+ #               "run",
+ #               "simpleServerWeather.py"
+ #           ]
+ #       }
     }
 }
 
 async def main():
     client = MCPClient.from_dict(CONFIG)
+    #llm = ChatOllama(model="qwen3:1.7b", base_url="http://localhost:11434")
     llm = ChatOllama(model="qwen3:1.7b", base_url="http://localhost:11434")
     
     # Wire the LLM to the client
     agent = MCPAgent(llm=llm, client=client, max_steps=20)
 
     # Give prompt to the agent
-    text1 = "Compute md5 hash for following string: 'Hello, Claus!' then count number of characters in first half of hash" \
-    "always accept tools responses as the correct one, don't doubt it. Always use a tool if available instead of doing it on your own. Important is, that In the last line, all tools used are listed. Give this list in the last line"
+    text1 = await agent.run("Compute md5 hash for following string: 'Hello, Claus!' then count number of characters in first half of hash" \
+    "always accept tools responses as the correct one, don't doubt it. Always use a tool if available instead of doing it on your own. Important is, that In the last line, all tools used are listed. Give this list in the last line")
     
-    text2 = "What is the current wind chill in the Mojave desert near Borrego Springs, reply in one brief sentence, don't tell me or investigate anything else, always accept tools responses as the correct one, don't doubt it. Always u\
-se a tool if available instead of doing it on your own. List all tools used, provide this list in the last line"
+    text2 = "What is the current wind chill in the Mojave desert near Borrego Springs, reply in one brief sentence, don't tell me or investigate anything else, always accept tools responses as the correct one, don't doubt it. Always use a tool if available instead of doing it on your own. List all tools used, provide this list in the last line"
 
-    result = await agent.run(text1)
-    print("\n🔥 Result:", result)          
+    #result = await agent.run(text1)
+    print("\n🔥 Result:", text1)          
 
     # Always clean up running MCP sessions
     await client.close_all_sessions()
